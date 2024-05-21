874c0c4c583117a54db2726ead0200b3ce25b1ad
PhuLoc25102003 <155551286+PhuLoc25102003@users.noreply.github.com>
2024-05-21 15:52:22 +0700

    add class ConfidentialDoc

---
+++ Documentation_Project/src/ConfidentialDoc.java
@@ -0,0 +1,23 @@
+public class ConfidentialDoc extends DocumentBuilder{
+
+    @Override
+    public Document buildDoc() {
+       
+        return new Document(extension, encyption);
+    }
+
+    @Override
+    public void setEncyption(boolean encyption) {
+        this.encyption = encyption;
+        
+    }
+
+    @Override
+    public void setExtension(String extension) {
+        this.extension = extension;
+        
+    }
+
+    
+
+}
