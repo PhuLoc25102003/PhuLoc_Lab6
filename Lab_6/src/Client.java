34f8899cacc669bf487ba7a37328ad93f7e88554
PhuLoc25102003 <155551286+PhuLoc25102003@users.noreply.github.com>
2024-05-21 14:53:04 +0700

    add class Client

---
+++ Lab_6/src/Client.java
@@ -0,0 +1,10 @@
+public class Client {
+    public static void main(String[] args) {
+        Contact createContact = new Contact("001", "002", "003", 0.0);
+
+        System.out.println(createContact);
+
+    }
+
+
+}
