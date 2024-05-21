0e4b3c06ac2f2a2ef86b60c7ed1b0358949c9d85
PhuLoc25102003 <155551286+PhuLoc25102003@users.noreply.github.com>
2024-05-21 15:52:55 +0700

    add class Document

---
+++ Documentation_Project/src/Document.java
@@ -0,0 +1,11 @@
+public class Document {
+    private String extension;
+    private boolean encyption;
+
+    public Document(String extension, boolean encyption) {
+        this.extension = extension;
+        this.encyption = encyption;
+    }
+  
+    
+}
