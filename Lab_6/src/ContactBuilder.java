dc9ab565ca93c351c1c3bc819688f1eca5109d59
PhuLoc25102003 <155551286+PhuLoc25102003@users.noreply.github.com>
2024-05-21 14:01:14 +0700

    update ContactBuilder ver_2

--- Lab_6/src/ContactBuilder.java
+++ Lab_6/src/ContactBuilder.java
@@ -1,9 +1,9 @@
 public interface ContactBuilder {
 
-    void buildContactID();
-    void buildPropertyID();
-    void buildTenantID();
-    void buildRentAmount();
+    void buildContactID(String contactID);
+    void buildPropertyID(String propertyID);
+    void buildTenantID(String tenantID);
+    void buildRentAmount(double rentAmount);
 
     Contact signalContact();
 
