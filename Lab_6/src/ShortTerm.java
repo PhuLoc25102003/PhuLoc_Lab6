b99e111f7845d08b50a397933544d4ca4b1a1253
PhuLoc25102003 <155551286+PhuLoc25102003@users.noreply.github.com>
2024-05-21 14:17:24 +0700

    Add class ShortTerm

---
+++ Lab_6/src/ShortTerm.java
@@ -0,0 +1,36 @@
+public class ShortTerm implements ContactBuilder{
+    private String contactID;
+    private double rentAmount;
+    private String tenantID;
+    private String propertyID;
+
+    @Override
+    public void buildContactID(String contactID) {
+       this.contactID = contactID;
+        
+    }
+
+    @Override
+    public void buildPropertyID(String propertyID) {
+        this.propertyID = propertyID;
+        
+    }
+
+    @Override
+    public void buildRentAmount(double rentAmount) {
+        this.rentAmount = rentAmount;
+        
+    }
+
+    @Override
+    public void buildTenantID(String tenantID) {
+        this.tenantID = tenantID;
+        
+    }
+
+    @Override
+    public Contact signalContact() {
+        return new Contact(contactID, propertyID, tenantID, rentAmount);
+    }
+
+}
