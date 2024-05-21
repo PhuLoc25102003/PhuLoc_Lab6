09ca631a2db24fbcedc1b70b7e574826de503ba0
PhuLoc25102003 <155551286+PhuLoc25102003@users.noreply.github.com>
2024-05-21 14:49:41 +0700

    update class Contact ver_2

--- Lab_6/src/Contact.java
+++ Lab_6/src/Contact.java
@@ -3,7 +3,7 @@ public class Contact {
     private String propertyID;
     private String tenantID;
     private double rentAmount;
-    
+
     public Contact(String contactID, String propertyID, String tenantID, double rentAmount) {
         this.contactID = contactID;
         this.propertyID = propertyID;
@@ -11,7 +11,10 @@ public class Contact {
         this.rentAmount = rentAmount;
     }
 
-    
+    @Override
+    public String toString(){
+        return this.contactID + " " + this.propertyID + " " + this.tenantID + " " + this.rentAmount + "\n";
+    }
 
     
 }
